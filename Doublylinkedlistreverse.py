cffb65814c3b767e330008df44b5254e10429f3b
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-03-27 15:32:01 +0530

    done

--- Doublylinkedlistreverse.py
+++ Doublylinkedlistreverse.py
@@ -1,48 +1,47 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-class LinkedList:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+class Linkedlist:
     def __init__(self):
-        self.head = None
-
-    def add(self, data):
-        newNode = Node(data)
+        self.head=None
+    def add(self,data):
+        newNode=Node(data)
         if self.head is None:
-            self.head = newNode
+            self.head=newNode
         else:
-            cur = self.head
+            cur=self.head
             while cur.next is not None:
-                cur = cur.next
-            cur.next = newNode
-            newNode.prev = cur
-
+                cur=cur.next
+            cur.next=newNode
+            newNode.prev=cur
     def print(self):
-        cur = self.head
+        cur=self.head
         while cur is not None:
-            print(cur.data, end=" ")
-            cur = cur.next
+            print(cur.data,end=" ")
+            cur=cur.next
         print()
-
-    def reverse(self):
-        cur = self.head
+        
+    def doublereverse(self):
+        cur=self.head
+        prev=None
+        
         while cur is not None:
-            temp = cur.next
-            cur.next = cur.prev
-            cur.prev = temp
-
-            if temp is None:
-                self.head = cur
-            cur = temp
-
-ob=LinkedList()
+            
+            temp=cur.next
+            cur.next=cur.prev
+            cur.prev=temp
+            prev=cur
+            cur=temp
+        self.head=prev
+        
+obj=Linkedlist()
 n=int(input())
 arr=list(map(int,input().split()))
 
 for i in arr:
-    ob.add(i)
-
-ob.reverse()
-ob.print()
+    obj.add(i)
+    
+obj.doublereverse()
+obj.print()
