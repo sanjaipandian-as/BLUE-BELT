65132be6ca6fe1a22d13fa0f43db258fb025c4bd
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-03-28 13:55:08 +0530

    done

--- Single Linkedlist reverese.py
+++ Single Linkedlist reverese.py
@@ -1,42 +1,48 @@
 class Node:
-    def _init_(self,data):
-        self.data=data
-        self.pointer=None
-class linkedlist:
-    def _init_(self):
-        self.head=None
-    def add(self,data):
-        newNode=Node(data)
+    def __init__(self, data):  # Fixed constructor name
+        self.data = data
+        self.next = None  # Changed pointer to next
+
+class LinkedList:
+    def __init__(self):  # Fixed constructor name
+        self.head = None
+
+    def add(self, data):
+        newNode = Node(data)
         if self.head is None:
-            self.head=newNode
+            self.head = newNode
         else:
-            cur=self.head
-            while cur.pointer is not None:
-                cur=cur.pointer
-            cur.pointer=newNode
+            cur = self.head
+            while cur.next is not None:  # Fixed pointer
+                cur = cur.next
+            cur.next = newNode  # Fixed pointer
+
     def print(self):
         if self.head is None:
-            print("linked list is empty")
+            print("Linked list is empty")
         else:
-            cur=self.head
+            cur = self.head
             while cur is not None:
-                print(cur.data,end=" ")
-                cur=cur.pointer
+                print(cur.data, end=" ")
+                cur = cur.next  # Fixed pointer
             print()
+
     def reverse(self):
-        prev=None
-        cur=self.head
+        prev = None
+        cur = self.head
         while cur is not None:
-            nextnode=cur.pointer
-            cur.pointer=prev
-            prev=cur
-            cur=nextnode
-        self.head=prev
-            
-obj=linkedlist()
-n=int(input())
-a=list(map(int,input().split()))
+            nextNode = cur.next  # Fixed pointer
+            cur.next = prev  # Fixed pointer
+            prev = cur
+            cur = nextNode
+        self.head = prev
+
+obj = LinkedList()
+n = int(input())  # Number of elements
+a = list(map(int, input().split()))  # List input
 for i in a:
     obj.add(i)
 obj.reverse()
 obj.print()
+
+input (n=5) and (arr=1,2,3,4,5)
